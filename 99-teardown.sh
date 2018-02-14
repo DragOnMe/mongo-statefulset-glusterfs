a69325fa64c5671b2d82540503e41565024f0307
DragOnMe <dragonme@gmail.com>
2018-02-14 15:21:07 +0900

    mod 99-teardown.sh - some variables

--- 99-teardown.sh
+++ 99-teardown.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 ##
-# Script to remove/undepoy all project resources from GKE & GCE.
+# Script to remove/undepoy all project resources from K8s cluster
 ##
 
-# Delete mongod stateful set + mongodb service + secrets + host vm configuer daemonset
-kubectl delete sts -n ns-mongo mongod-ss
-kubectl delete svc -n ns-mongo mongodb-hs
-kubectl delete secret -n ns-mongo shared-bootstrap-data
+# Variables
+MONGOD_STATEFULSET="mongod-ss"
+MONGOD_NAMESPACE="ns-mongo"
+
+# Delete mongod stateful set, mongodb service, secrets
+kubectl delete sts -n ${MONGOD_NAMESPACE} ${MONGOD_STATEFULSET}
+kubectl delete svc -n ${MONGOD_NAMESPACE} mongodb-hs
+kubectl delete secret -n ${MONGOD_NAMESPACE} shared-bootstrap-data
 sleep 3
 
 # Delete persistent volume claims
-kubectl delete pvc -n ns-mongo -l role=mongo
+kubectl delete pvc -n ${MONGOD_NAMESPACE} -l role=mongo
+
+# Delete mongoclient pod & deployment
+kubectl delete deployment mongo-client -n ${MONGOD_NAMESPACE}
 
 # Delete namespace
-kubectl delete ns ns-mongo
+kubectl delete ns ${MONGOD_NAMESPACE}
